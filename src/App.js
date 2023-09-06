2e0b5a8ff63bd5df3c55bd2378f7cebc7e0ef081
ajroudirida <hpwin10kamilio@gmail.com>
2023-09-06 12:12:32 +0100

    missions setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Profile from './components/profile/Profile';
 import Rockets from './components/rockets/Rockets';
+import Missions from './components/missions/Missions';
 import Header from './components/header/Header';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Rockets />} />
+            <Route path="/missions" element={<Missions />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
---
+++ src/components/missions/Missions.js
@@ -0,0 +1,15 @@
+import './missions.css';
+
+const Missions = () => (
+  <div className="missions-list-table" data-testid="missions">
+    <div className="table-header">
+      <div className="th th-1">Mission</div>
+      <div className="th th-2">Description</div>
+      <div className="th th-3">Status</div>
+      <div className="th th-4" />
+    </div>
+    <div className="table-body" />
+  </div>
+);
+
+export default Missions;
