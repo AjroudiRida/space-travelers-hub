344552098eb698f08c214b6b7ad5b7fdff544ae3
ajroudirida <hpwin10kamilio@gmail.com>
2023-09-06 15:42:09 +0100

    display reserved rockets

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,19 @@
-import './profile.css';
-import { useSelector } from 'react-redux';
+import "./profile.css";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const rockets = useSelector((state) => state.rockets);
   const missions = useSelector((state) => state.missions);
+  const reservedRockets = rockets.rockets.filter((rocket) => {
+    if ("reserved" in rocket && rocket.reserved) {
+      return true;
+    }
+
+    return false;
+  });
 
   const joinedMissions = missions.missions.filter((mission) => {
-    if ('reserved' in mission && mission.reserved) {
+    if ("reserved" in mission && mission.reserved) {
       return true;
     }
 
@@ -21,10 +29,14 @@ const Profile = () => {
             {mission.mission_name}
           </div>
         ))}
-
       </div>
       <div className="my-rockets">
         <h2 className="rockets-title">My Rockets</h2>
+        {reservedRockets.map((rocket) => (
+          <div className="rocket" key={rocket.id}>
+            {rocket.name}
+          </div>
+        ))}
       </div>
     </div>
   );
