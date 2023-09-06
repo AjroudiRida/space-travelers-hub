4215ec5d10b3d97562e7a6fd80e917ed8585d519
NGALA MAC <macngala4@gmail.com>
2023-09-06 13:50:08 +0100

    Add rocket display component

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,3 +1,24 @@
-const Rockets = () => <>rockets</>;
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Rocket from '../rocket/Rocket';
+import { getRockets } from '../../redux/features/rockets/rocketsSlice';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rocketsState = useSelector(
+    (state) => state.rockets,
+  );
+  useEffect(() => {
+    dispatch(getRockets('rockets'));
+  }, [dispatch]);
+
+  return (
+    <>
+      {rocketsState.rockets.map((rocket) => (
+        <Rocket key={rocket.id} rocket={rocket} />
+      ))}
+    </>
+  );
+};
 
 export default Rockets;
